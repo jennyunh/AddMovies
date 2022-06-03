5e852e41029f53db1e0e00038ebff7f8d480013d
jennyunh <jennyunh@gmail.com>
2022-06-02 23:13:53 -0700

    Updates

---
+++ precache-manifest.baf9955405142af9ed9da396c1665c5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9ceb88a14a148d55df61fb6f0042d7",
+    "url": "/AddMovies/index.html"
+  },
+  {
+    "revision": "2ad6342e1b42baaa97d6",
+    "url": "/AddMovies/static/css/main.29750fd1.chunk.css"
+  },
+  {
+    "revision": "f0c76ba57cd4efe4fb2e",
+    "url": "/AddMovies/static/js/2.c2ec4765.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/AddMovies/static/js/2.c2ec4765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad6342e1b42baaa97d6",
+    "url": "/AddMovies/static/js/main.a1b916b7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/AddMovies/static/js/main.a1b916b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32fcd41ce53033594cd",
+    "url": "/AddMovies/static/js/runtime-main.c4555368.js"
+  }
+]);
